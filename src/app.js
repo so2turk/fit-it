9e7f346faa9c990ce8b877b5905458e9f7ca153d
Serhat Ozturk <so2turk@yahoo.com>
2022-05-30 20:34:22 +0200

    Initial setup

---
+++ src/app.js
@@ -0,0 +1,13 @@
+import './app.css'
+import FitIt from './components/fit-it'
+
+function App() {
+	return (
+		<div className="app">
+			<FitIt />
+			<div></div>
+		</div>
+	)
+}
+
+export default App
---
+++ src/components/fit-it.js
@@ -0,0 +1,5 @@
+const FitIt = () => {
+	return <div></div>
+}
+
+export default FitIt
