a4c69d9b13f5e47b3c7bbc241515786936358de6
Serhat Ozturk <so2turk@yahoo.com>
2022-05-30 22:08:58 +0200

    Setup input field

--- src/components/__test__/fit-it.test.js
+++ src/components/__test__/fit-it.test.js
@@ -1,4 +1,36 @@
-import { render, screen } from '@testing-library/react'
+import { fireEvent, render, screen } from '@testing-library/react'
 import FitIt from '../fit-it'
 
 const setup = () => render(<FitIt />)
+
+it('should have a title', () => {
+	setup()
+	expect(screen.getByTestId('title').textContent).toBe('Fit-It')
+})
+
+it('should have an input field', () => {
+	setup()
+	expect(screen.getByTestId('input').value).toBe('')
+})
+
+it('should have an add button', () => {
+	setup()
+	expect(screen.getByTestId('add-btn').textContent).toBe('Add')
+})
+
+it('should have an evaluate button', () => {
+	setup()
+	expect(screen.getByTestId('eva-btn').textContent).toBe('Fit It')
+})
+
+test('should change input value', () => {
+	setup()
+	const inputEl = screen.getByTestId('input')
+
+	fireEvent.change(inputEl, {
+		target: {
+			value: 'fit-it',
+		},
+	})
+	expect(inputEl.value).toBe('fit-it')
+})
--- src/components/fit-it.js
+++ src/components/fit-it.js
@@ -1,5 +1,12 @@
 const FitIt = () => {
-	return <div></div>
+	return (
+		<>
+			<h1 data-testid="title">Fit-It</h1>
+			<input data-testid="input" />
+			<button data-testid="add-btn">Add</button>
+			<button data-testid="eva-btn">Fit It</button>
+		</>
+	)
 }
 
 export default FitIt
